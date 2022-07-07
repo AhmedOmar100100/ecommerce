a96e2ed60861c47b35f34c0694dfcbbbd5e6f2fe
AhmedOmar100100 <ahmedomar234_sd@fcis.bsu.edu.eg>
2022-07-07 15:24:49 +0200

    7/7/2022 single-product page

--- js/main.js
+++ js/main.js
@@ -14,3 +14,22 @@ if (close) {
         nav.classList.remove("active")
     })
 }
+
+// single product slider
+
+let mainImage = document.getElementById("main-image")
+let smallImages = document.getElementsByClassName("small-image")
+
+smallImages[0].onclick = function () {
+    mainImage.src = smallImages[0].src;
+}
+smallImages[1].onclick = function () {
+    mainImage.src = smallImages[1].src;
+}
+smallImages[2].onclick = function () {
+    mainImage.src = smallImages[2].src;
+}
+smallImages[3].onclick = function () {
+    mainImage.src = smallImages[3].src;
+}
+
